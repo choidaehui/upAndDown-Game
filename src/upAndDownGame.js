f34d026406e041c6952705ae10747985fee65331
DaeHui <77823790+choidaehui@users.noreply.github.com>
2021-08-07 01:11:09 +0900

    upAndDownGame: update

--- src/upAndDownGame.js
+++ src/upAndDownGame.js
@@ -2,31 +2,42 @@
 const randomNum = Math.floor(Math.random()*100)+1;
 console.log(randomNum);
 let count = 0;
-let result = document.querySelector('#result');
-const inputNum = document.querySelector('#randomNum');
 
-function numCal(inputNum) {
-  if (inputNum.innerHTML < 1 || inputNum.innerHTML > 100){
+const enterCheck = function(e) {
+  if(e.keyCode == 13){
+      numCal();
+      return false;
+  }
+}
+
+document.querySelector('#randomNum').onkeypress = enterCheck;
+
+function numCal() {
+  let inputNum = document.querySelector('#randomNum').value;
+  let result = document.querySelector('#result')
+  
+  if (inputNum < 1 || inputNum > 100){
     alert("1부터 100까지 숫자를 입력해주세요.");
-    result.innerHTML = "";
   }
   else if (inputNum > randomNum) {
     alert("숫자 DOWN");
     count++;
-    result.innerHTML = `${count}번 시도`;
+    result.value = `${count}번 시도`;
   }
   else if (inputNum < randomNum) {
     alert("숫자 UP");
     count++;
-    result.innerHTML = `${count}번 시도`;
+    result.value = `${count}번 시도`;
   }
   else {
     alert("정답 입니다.");
     count++;
-    result.innerHTML = `${count}번 시도`;
+    result.value = `${count}번 시도 후에 정답을 맞혔습니다.`;
   }
+    document.querySelector('#randomNum').value = "";
+
 }
 
-document.addEventListener('click', () => numCal(inputNum) );
+
 
 
